10d8abc0c16457ff7fe548b3469d711f50b058a1
Diana Aguiñaga <dianacarolina.ag14@gmail.com>
2025-06-03 11:23:36 -0600

    added sorting to locations so to match states, created a table

--- script.js
+++ script.js
@@ -3,36 +3,47 @@ import { universityInformation } from "./JASONdata.js";
 const locationsTransformer = (jsonData) => {
   const features = jsonData.features;
 
-  const states = [];
-  features.forEach((f) => {
-    const state = f.attributes.State;
-    if (!states.includes(state)) {
-      states.push(state);
+  const locations = features.map((f) => ({
+    state: f.attributes.State,
+    name: f.attributes.University_Chapter,
+    lat: f.geometry.y,
+    lng: f.geometry.x,
+  }));
+
+  // Sort by state, then by name
+  locations.sort((a, b) => {
+    if (a.state === b.state) {
+      return a.name.localeCompare(b.name);
     }
+    return a.state.localeCompare(b.state);
   });
 
-  const locations = features.map((f) => {
-    return {
-      name: f.attributes.University_Chapter,
-      latLng: [f.geometry.y, f.geometry.x],
-    };
-  });
-
-  return [states, locations];
+  return locations;
 };
 
-const [states, locations] = locationsTransformer(universityInformation);
+const locations = locationsTransformer(universityInformation);
 
-const statesList = document.getElementById("states");
-states.forEach((state) => {
-  const li = document.createElement("li");
-  li.textContent = state;
-  statesList.appendChild(li);
-});
+const table = document.getElementById("states-locations-table");
+const tbody = table.tBodies[0];
 
-const locationsList = document.getElementById("locations");
 locations.forEach((loc) => {
-  const li = document.createElement("li");
-  li.textContent = `${loc.name} [${loc.latLng[0]}, ${loc.latLng[1]}]`;
-  locationsList.appendChild(li);
+  const tr = document.createElement("tr");
+
+  const tdState = document.createElement("td");
+  tdState.textContent = loc.state;
+  tr.appendChild(tdState);
+
+  const tdName = document.createElement("td");
+  tdName.textContent = loc.name;
+  tr.appendChild(tdName);
+
+  const tdLat = document.createElement("td");
+  tdLat.textContent = loc.lat;
+  tr.appendChild(tdLat);
+
+  const tdLng = document.createElement("td");
+  tdLng.textContent = loc.lng;
+  tr.appendChild(tdLng);
+
+  tbody.appendChild(tr);
 });
